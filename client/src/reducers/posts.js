5099ceb1220fa75e7e50db7b1e686608ac27b225
Joe Jung <joejung5678@gmail.com>
2021-02-27 15:51:00 -0500

    Completed up to Google OAuth

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -1,15 +1,17 @@
+import {FETCH_ALL, CREATE, UPDATE, LIKE, DELETE} from '../constants/actionTypes';
+
 /* eslint-disable import/no-anonymous-default-export */
 export default (posts=[],action) => {
     switch(action.type) {
-        case 'FETCH_ALL':
+        case FETCH_ALL:
             console.log('action.payload: ' + action.payload);
             return action.payload;
-        case 'CREATE':
+        case CREATE:
             return [...posts, action.payload];
-        case 'UPDATE':
-        case 'LIKE':
+        case UPDATE:
+        case LIKE:
             return posts.map((post)=> post._id === action.payload._id ? action.payload : post);
-        case 'DELETE':
+        case DELETE:
             return posts.filter((post)=> post._id !== action.payload);
         default:
             return posts;
